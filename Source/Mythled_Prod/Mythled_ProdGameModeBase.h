e6dbfc9f9b44bee84ccf689039eba58380f9a031
VacherCamille <39760408+VacherCamille@users.noreply.github.com>
2020-01-15 13:42:12 -0500

    Creation

---
+++ Source/Mythled_Prod/Mythled_ProdGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Mythled_ProdGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYTHLED_PROD_API AMythled_ProdGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
