5f3807821ea85d06b0169259fbd34c1168526455
tiwentou <57150227+tiwentou@users.noreply.github.com>
2020-02-14 12:33:00 -0500

    Menu pause et tout laaaaa

--- Source/Mythled_Prod/CharMovement.cpp
+++ Source/Mythled_Prod/CharMovement.cpp
@@ -5,6 +5,7 @@
 #include "Runtime/Engine/Public/TimerManager.h"
 #include "Kismet/GameplayStatics.h"
 #include "Engine.h"
+#include "Blueprint/UserWidget.h"
 
 // Sets default values
 ACharMovement::ACharMovement()
