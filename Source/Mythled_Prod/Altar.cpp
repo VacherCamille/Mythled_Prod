cfeaa99d06291fb56979f2f029927a7e30cf3f4a
tiwentou <57150227+tiwentou@users.noreply.github.com>
2020-02-19 11:06:44 -0500

    Son autel c++

--- Source/Mythled_Prod/Altar.cpp
+++ Source/Mythled_Prod/Altar.cpp
@@ -22,12 +22,25 @@ AAltar::AAltar()
 
 	HitBox->OnComponentBeginOverlap.AddDynamic(this, &AAltar::OnOverlapBegin);
 	HitBox->OnComponentEndOverlap.AddDynamic(this, &AAltar::OnOverlapEnd);
+
+	//sound
+	static ConstructorHelpers::FObjectFinder<USoundCue> ActivationSoundCueObject(TEXT("SoundCue'/Game/Objects/Sound_effects/Altar/AltarCue.AltarCue'"));
+	if (ActivationSoundCueObject.Succeeded()) {
+		ActivationSoundCue = ActivationSoundCueObject.Object;
+
+		ActivationAudioComponent = CreateDefaultSubobject<UAudioComponent>(TEXT("ActivationAudioComponent"));
+		ActivationAudioComponent->SetupAttachment(RootComponent);
+	}
 }
 
 // Called when the game starts or when spawned
 void AAltar::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if (ActivationAudioComponent && ActivationSoundCue) {
+		ActivationAudioComponent->SetSound(ActivationSoundCue);
+	}
 	
 }
 
@@ -57,11 +70,17 @@ void AAltar::OnOverlapEnd(UPrimitiveComponent * OverlappedComp, AActor * OtherAc
 	}
 }
 
-void AAltar::ChangeIsActivated()
+void AAltar::ChangeIsActivated() 
 {
+	
 	isActivated = !isActivated;
 	if (Door) {
-		if (isActivated) Door->OpenDoor();
+		if (isActivated) {
+			Door->OpenDoor();
+			if (ActivationAudioComponent && ActivationSoundCue) {
+				ActivationAudioComponent->Play(0.f);
+			}
+		}
 		else Door->CloseDoor();
 	}
 }
